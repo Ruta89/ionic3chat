3b08e84c078c0b3d2dffc8a108a22b8b7c045e1e
Arek Ruta <arek.ruta89@gmail.com>
2017-07-11 23:08:24 +0200

    requests

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,5 +1,6 @@
+import { RequestsProvider } from './../../providers/requests/requests';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -7,17 +8,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'chats.html',
 })
 export class ChatsPage {
+  myRequests;
 
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public requestService: RequestsProvider,
+    public events: Events) {
+  }
+
+  ionViewWillEnter() {
+    this.requestService.getMyRequest();
+    this.events.subscribe('gotrequests', () => {
+      this.myRequests = [];
+      this.myRequests = this.requestService.userDetails;
+      //  console.log('his.events.subscribe(gotRequests this.requestService.userDetails  ', this.requestService.userDetails);
+    })
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave  this.events.unsubscribe( gotrequests );  ', this.requestService.userDetails);
+    this.events.unsubscribe('gotrequests');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChatsPage');
   }
 
-  addbuddy(){
+  addbuddy() {
     this.navCtrl.push('BuddiesPage');
+  }
+  accept(item){
+    console.log('Zaakceptowales: ', item.displayName);
+  }
+
+  ignore(item){
+    console.log('Ignorujesz  ', item.displayName);
+    alert('Ignorujesz  ' + item.displayName)
   }
 
 }
