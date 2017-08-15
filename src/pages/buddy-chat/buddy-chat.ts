596cf26bf051e7d44f4a732edad21b84e711c2dc
Arek Ruta <arek.ruta89@gmail.com>
2017-08-15 15:32:19 +0200

    add photo message and refactor

--- src/pages/buddy-chat/buddy-chat.ts
+++ src/pages/buddy-chat/buddy-chat.ts
@@ -1,3 +1,4 @@
+import { ImghandlerProvider } from './../../providers/img-handler/img-handler';
 import { ChatProvider } from './../../providers/chat/chat';
 import { Component, ViewChild, NgZone } from '@angular/core';
 import {
@@ -5,9 +6,10 @@ import {
   NavController,
   NavParams,
   Events,
-  Content
+  Content,
+  LoadingController
 } from 'ionic-angular';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 
 @IonicPage()
 @Component({
@@ -17,25 +19,32 @@ import * as firebase from 'firebase/app';
 export class BuddyChatPage {
   @ViewChild('content') content: Content;
   buddy: any;
-  message;
   newMessage;
   allMessages = [];
   photoURL;
-
+  imgornot;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public chatService: ChatProvider,
     public events: Events,
-    public zone: NgZone
+    public zone: NgZone,
+    public loadingCtrl: LoadingController,
+    public imgStore: ImghandlerProvider
   ) {
     this.buddy = this.chatService.buddy;
     this.photoURL = firebase.auth().currentUser.photoURL;
     this.scrollTo();
     this.events.subscribe('newMessage', () => {
       this.allMessages = [];
+      this.imgornot = [];
       this.zone.run(() => {
         this.allMessages = this.chatService.buddyMessages;
+        for (var key in this.allMessages) {
+          if (this.allMessages[key].message.substring(0, 4) == 'http')
+            this.imgornot.push(true);
+          else this.imgornot.push(false);
+        }
       });
     });
   }
@@ -59,5 +68,25 @@ export class BuddyChatPage {
     setTimeout(() => {
       this.content.scrollToBottom();
     }, 1000);
+  }
+
+  sendPicMsg() {
+    let loader = this.loadingCtrl.create({
+      content: 'Proszę czekac'
+    });
+    loader.present();
+    this.imgStore
+      .picmsgstore()
+      .then(imgUrl => {
+        loader.dismiss();
+        this.chatService.addNewMessage(imgUrl).then(() => {
+          this.scrollTo();
+          this.newMessage = '';
+        });
+      })
+      .catch(err => {
+        alert(err);
+        loader.dismiss();
+      });
   }
 }
