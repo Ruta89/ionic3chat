10db017a8b55413eaf5c435e90f3b3817e9ddff8
Arek Ruta <arek.ruta89@gmail.com>
2017-09-26 01:47:44 +0200

    view(allMyGroups), addGroup, openChat
    groups Page
     view list of allMyGroups
     addGroup
     openChat

--- src/pages/groups/groups.ts
+++ src/pages/groups/groups.ts
@@ -1,18 +1,53 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  Events,
+  LoadingController
+} from 'ionic-angular';
+import { GroupsProvider } from './../../providers/groups/groups';
 @IonicPage()
 @Component({
   selector: 'page-groups',
-  templateUrl: 'groups.html',
+  templateUrl: 'groups.html'
 })
 export class GroupsPage {
+  allMyGroups;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public events: Events,
+    public groupService: GroupsProvider
+  ) {}
+
+  ionViewWillEnter() {
+    let loader = this.loadingCtrl.create({
+      content: 'Pobieranie grup, czekaj ...'
+    });
+    loader.present();
+    this.groupService.getMyGroups();
+    loader.dismiss();
+    this.events.subscribe('allMyGroups', () => {
+      this.allMyGroups = this.groupService.myGroups;
+    });
+  }
+
+  ionViewDidLeave() {
+    this.events.unsubscribe('allMyGroups');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GroupsPage');
   }
 
+  addGroup() {
+    this.navCtrl.push('NewgroupPage');
+  }
+
+  openChat(group) {
+    alert('GroupChat ' + group.groupName);
+  }
 }
