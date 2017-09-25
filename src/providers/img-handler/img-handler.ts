fac1b936fddc106baad51a33e4e2e3956de1abdb
Arek Ruta <arek.ruta89@gmail.com>
2017-09-26 01:50:44 +0200

    ImghandlerProvider
    img-handler Provider
     groupPicStore

--- src/providers/img-handler/img-handler.ts
+++ src/providers/img-handler/img-handler.ts
@@ -110,4 +110,48 @@ export class ImghandlerProvider {
       s4()
     );
   }
+
+  groupPicStore(groupName) {
+    var promise = new Promise((resolve, reject) => {
+      this.filechooser.open().then(url => {
+        (<any>window).FilePath.resolveNativePath(url, result => {
+          this.nativepath = result;
+          (<any>window).resolveLocalFileSystemURL(this.nativepath, res => {
+            res.file(resFile => {
+              var reader = new FileReader();
+              reader.readAsArrayBuffer(resFile);
+              reader.onloadend = (evt: any) => {
+                var imgBlob = new Blob([evt.target.result], {
+                  type: 'image/jpeg'
+                });
+                var imageStore = this.firestore
+                  .ref('/groupImages')
+                  .child(firebase.auth().currentUser.uid)
+                  .child(groupName);
+                imageStore
+                  .put(imgBlob)
+                  .then(res => {
+                    this.firestore
+                      .ref('/profileimages')
+                      .child(firebase.auth().currentUser.uid)
+                      .child(groupName)
+                      .getDownloadURL()
+                      .then(url => {
+                        resolve(url);
+                      })
+                      .catch(err => {
+                        reject(err);
+                      });
+                  })
+                  .catch(err => {
+                    reject(err);
+                  });
+              };
+            });
+          });
+        });
+      });
+    });
+    return promise;
+  }
 }
