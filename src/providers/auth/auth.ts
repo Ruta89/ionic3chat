2301a4b96e3fb46cd34ced94bcbc1ca3ee304001
Arek Ruta <arek.ruta89@gmail.com>
2017-09-28 14:37:29 +0200

    some fixes

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,38 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { usercreds } from '../../models/interfaces/usercreds';
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 
 @Injectable()
 export class AuthProvider {
   authState: Observable<firebase.User>;
   user: firebase.User;
-  
-  constructor(public afireauth: AngularFireAuth) {
+
+  constructor(private afireauth: AngularFireAuth) {
     this.authState = afireauth.authState;
 
-    this.authState.subscribe(user =>{
+    this.authState.subscribe(user => {
       this.user = user;
-    })
+    });
   }
-
-  /*
-      For logging in a particular user. Called from the login.ts file.
-    
-  */
 
   login(credentials: usercreds) {
-    var promise = new Promise((resolve, reject) => {
-      this.afireauth.auth.signInWithEmailAndPassword(credentials.email, credentials.password).then(() => {
-        resolve(true);
-      }).catch((err) => {
-        reject(err);
-      })
-    })
-
-    return promise;
-
+    return this.afireauth.auth.signInWithEmailAndPassword(credentials.email, credentials.password);
   }
-
 }
